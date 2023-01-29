c1675f1bf7f92cdf461214a4dff590ee99ea079f
Theodore Otzenberger <totzenberger@palantir.com>
2023-01-29 14:14:49 +0100

    async param

--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ def get_http_method_name(d: dict) -> str:
     return next(iter(d))
 
 
-def get_sub_dict(d: dict, http_method: str) -> dict:
+def get_sub_dict(d: dict,
+                 http_method: str) -> dict:
     return d[http_method]
 
 
@@ -26,7 +27,8 @@ def get_dict_of_parameter(sub_dict: dict):
     return {"name": param["name"], "type": param["schema"]["type"]}
 
 
-def all_needed_info_on_endpoint(all_paths, endpoint):
+def all_needed_info_on_endpoint(all_paths,
+                                endpoint: str):
     path = all_paths[endpoint]
     http_method = get_http_method_name(path)
     sub_dict = get_sub_dict(path, http_method=http_method)
@@ -48,7 +50,7 @@ def get_all_info_from_json(json_url: str):
     list_info = []
     for path in all_paths.keys():
         info = all_needed_info_on_endpoint(all_paths, path)
-        if info["tag_name"] == None:
+        if info["tag_name"] is None:
             continue
         list_info.append(info)
     return list_info
@@ -64,35 +66,42 @@ def create_stringified_function_name(info_endpoint: dict):
     if parameters_string != '':
         parameters_string += ", "
     func_name = "async def " + info_endpoint[
-        "function_name"] + "(" + parameters_string + "cookies: dict = None, base_url: str = base_url, endpoint: str = '" + info_endpoint[
-                    "endpoint"] + "'):"
+        "function_name"] + "(" + parameters_string + "cookies: dict = None, base_url: str = base_url, " \
+                                                     "endpoint: str = '" + info_endpoint["endpoint"] + "'):"
+
     return func_name
 
 
-def create_stringified_function_request(info_endpoint: dict):
-    base = "\ttry: \n\t\tres = await client." + info_endpoint["http_method"] + "(url=base_url+endpoint"
+def create_stringified_function_request(info_endpoint: dict, async_client: bool):
+    if async_client:
+        client = "await client."
+    else:
+        client = "httpx."
+    base = f"\ttry: \n\t\tres = {client}" + info_endpoint["http_method"] + "(url=base_url+endpoint"
     if (info_endpoint["http_method"] == "post") or (info_endpoint["http_method"] == "put"):
         body_stringified = ", json=jsonable_encoder(data)"
     else:
         if info_endpoint["params"]:
             body_stringified = "+ '/'" + f"+{info_endpoint['params']['name']} \n"
         else:
-            body_stringified = ", cookies=cookies)\n\t\tif res.status_code >= 300:\n\t\t\traise HTTPException(status_code=res.status_code) \n) \n"
+            body_stringified = ", cookies=cookies)\n\t\tif res.status_code >= 300:\n\t\t\traise HTTPException(" \
+                               "status_code=res.status_code) \n) \n"
     base += body_stringified
     base += "\texcept httpx.HTTPError as err: \n\t\traise SystemExit(err)"
     base += "\n\treturn res.json()"
     return base
 
 
-def build_whole_python_function(func_header: str, func_body: str):
+def build_whole_python_function(func_header: str,
+                                func_body: str):
     return func_header + "\n" + func_body
 
 
-def build_all_functions_from_info(all_info: list) -> dict:
+def build_all_functions_from_info(all_info: list, async_client: bool) -> dict:
     dict_tags_functions = {}
     for info in all_info:
         entire_function = build_whole_python_function(create_stringified_function_name(info),
-                                                      create_stringified_function_request(info))
+                                                      create_stringified_function_request(info, async_client=async_client))
         if info["tag_name"] in dict_tags_functions:
             dict_tags_functions[info["tag_name"]] += [entire_function]
         else:
@@ -100,16 +109,7 @@ def build_all_functions_from_info(all_info: list) -> dict:
     return dict_tags_functions
 
 
-def from_json_to_functions(json_url: str) -> dict:
+def from_json_to_functions(json_url: str, async_client: bool) -> dict:
     all_info = get_all_info_from_json(json_url=json_url)
-    return build_all_functions_from_info(all_info=all_info)
+    return build_all_functions_from_info(all_info=all_info, async_client=async_client)
 
-
-def write_functions_to_python_file_with_path(path_to_write_in: str, func_dict: dict, base_imports: str):
-    for key in func_dict.keys():
-        final_path = path_to_write_in + "/" + key + "_req.py"
-        to_write = "\n\n\n".join(func_dict[key])
-        with open(final_path, 'w') as f:
-            f.write(base_imports + "\n\n\n")
-            f.write(to_write)
-            f.close
